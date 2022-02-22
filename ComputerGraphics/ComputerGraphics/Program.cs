eb805dc278f6bb944f74647d35987f88b0d35746
Nelsoniwe <54241019+Nelsoniwe@users.noreply.github.com>
2022-02-22 21:30:20 +0200

    init

---
+++ ComputerGraphics/ComputerGraphics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComputerGraphics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
